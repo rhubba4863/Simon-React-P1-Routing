dae0652e4c2904f606d545c4666165af44149df8
rhubba4863 <rph_1994@yahoo.com>
2025-02-05 17:12:28 -0700

    Converted to React Components

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import './play.css';
+
 
 export function Play() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>play displayed here</div>
+    // <main className='container-fluid bg-secondary text-center'>
+    //   <div>play displayed here</div>
+    // </main>
+
+    <main class="bg-secondary">
+      <div class="players">
+        Player
+        <span class="player-name">Mystery player</span>
+        <div id="player-messages">
+          <div class="event"><span class="player-event">Linus</span> scored 377</div>
+          <div class="event"><span class="player-event">Linus</span> started a new game</div>
+          <div class="event"><span class="system-event">game</span> connected</div>
+        </div>
+      </div>
+
+      <div class="game">
+        <div class="button-container">
+          <button class="button-top-left"></button>
+          <button class="button-top-right"></button>
+          <button class="button-bottom-left"></button>
+          <button class="button-bottom-right"></button>
+          <div class="controls center">
+            <div class="game-name">Simon<sup>&reg;</sup></div>
+            <div class="score center">--</div>
+            <button class="btn btn-primary">Reset</button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
